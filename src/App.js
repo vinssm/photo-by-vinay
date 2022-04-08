e7640fb3ed04d155e6dc95a2863ded468896484d
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-07 20:55:30 -0400

    home page images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-//import { AppBar, Divider } from "@material-ui/core";
+// import {  Divider } from "@material-ui/core";
 import React from "react";
-import { Typography, AppBar, Toolbar } from "@material-ui/core";
+import { Typography, AppBar, Toolbar, Container } from "@material-ui/core";
+import HomeGallery from './HomeGallery';
+// import Photography from '../assets/images/Photography.jpg'
+
 
 const App = () => {
   return (
@@ -10,6 +13,14 @@ const App = () => {
             <Typography variant="h4">Photo by Vinay</Typography>
           </Toolbar>
         </AppBar>
+
+        <Container style={{"marginTop":"70px", "textAlign":"center"}}>
+            <Typography variant="h2">Photography Portfolio with React</Typography>         
+        </Container>
+
+        <Container maxWidth="lg">
+          <HomeGallery />            
+        </Container>
     </div>
   )
 }
