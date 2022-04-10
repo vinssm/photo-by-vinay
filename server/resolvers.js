946a22a75651c7430d1f2ce6f13086677ca3ffde
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-10 14:07:40 -0400

    added resolvers and schemas

---
+++ server/resolvers.js
@@ -0,0 +1,53 @@
+
+import {quotes, users} from './fakedb.js'
+import {randomBytes} from 'crypto'
+import mongoose from 'mongoose'
+
+const User = mongoose.model("User")
+import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from './config.js'
+
+const resolvers = {
+    Query:{
+        users:()=>users,
+        user:(_,{_id})=>users.find(user=>user._id == _id),
+        quotes:()=>quotes,
+        iquote:(_,{by})=>quotes.filter(quote=>quote.by == by)      
+    },
+    User:{
+        quotes:(ur)=>quotes.filter(quote=>quote.by == ur._id)
+    },
+    Mutation:{
+        signupUser:async (_,{userNew})=>{
+           const user = await User.findOne({email:userNew.email})
+           if(user){
+               throw new Error("User already exists with that email")
+           }
+       const hashedPassword = await bcrypt.hash(userNew.password,12)
+
+      const newUser = new User({
+            ...userNew,
+            password:hashedPassword
+       })
+
+         return await newUser.save()
+        },
+        signinUser:async (_,{userSignin})=>{
+            //User Signing In
+           const user = await User.findOne({email:userSignin.email})
+           if(!user){
+               throw new Error("User does not exist with that email")               
+           }
+           const doMatch = await bcrypt.compare(userSignin.password,user.password)
+           if(!doMatch){
+               throw new Error("email or password are invalid")  
+           }
+
+           const token = jwt.sign({userId:user._id},JWT_SECRET)
+           return {token}
+         },
+    }
+}
+
+export default resolvers
---
+++ server/schemaGql.js
@@ -0,0 +1,46 @@
+import {gql} from "apollo-server"
+
+
+const typeDefs = gql`
+type Query{
+   users: [User]
+   user(_id:ID!):User
+   quotes:[Quote]
+   iquote(by:ID!):[Quote]
+}
+
+type User{
+    _id:ID!
+    firstName:String
+    lastName:String
+    email:String
+    password:String
+    quotes:[Quote]
+}
+type Quote{
+    name:String,
+    by:ID
+}
+
+type Token{
+    token:String
+}
+
+type Mutation{
+    signupUser(userNew:UserInput!):User,
+    signinUser(userSignin:UserSigninInput!):Token
+}
+
+input UserInput{
+    firstName:String!,
+    lastName:String!,
+    email:String!,
+    password:String!
+}
+input UserSigninInput{
+    email:String!,
+    password:String!
+}
+`
+
+export default typeDefs
