b66d69fadad99cacd64e2a7d14ca90d3282f91c1
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-13 23:45:13 -0400

    heroku deploy fixed issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/Photo-by-Vinay',
+  process.env.MONGODB_URI || 'mongodb://localhost/Photo-by-Vinay',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
