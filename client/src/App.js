7be42712485d6ee5566a1394139b554075e9bb57
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-15 10:31:04 -0400

    deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,35 @@ import './App.css'
 import NavBar from "./components/NavBar";
 import {routes} from "./routes";
 import {useRoutes} from 'react-router';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
 
 function App() {
   const element = useRoutes(routes)
   return (
-      <div>
-          <NavBar />
-          <Container style={{"marginTop":"20px","textAlign":"center"}}>
-              <Typography variant="h2">Photography Portfolio with React</Typography>         
-          </Container>
-          {element}         
-     </div>
-  )
-  
+    <ApolloProvider client={client}>
+        <div>
+            <NavBar />
+            <Container style={{"marginTop":"20px","textAlign":"center"}}>
+                <Typography variant="h2">Photography Portfolio with React</Typography>         
+            </Container>
+            {element}         
+        </div>
+     </ApolloProvider>
+    )  
 }
 
 export default App;
