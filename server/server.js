d826fb66dac1b6c75bfea51a8eac75f811a3f8c5
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-15 23:54:13 -0400

    issues fixing for heroku deploy

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('./utils/auth');
 //const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
