756d623edf3674d09caeb683808ba920978b5773
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-04-15 23:24:35 -0400

    issues fixing for heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/photobyvinay', 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true,
+  useFindAndModify: true
 });
 
 module.exports = mongoose.connection;
